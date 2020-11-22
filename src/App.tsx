b17a25b719a210db0749048166ddc2a67ee21ee3
talmewho <talmewho@gmail.com>
2020-11-22 18:46:30 +0200

    Added react-router-dom and added a basic structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 export const App: React.FC = () => {
   return (
-    <div>Create React App</div>
+    <>
+      <h1 className="main-title">Sky Go</h1>
+      <Router>
+        <Switch>
+          <Route path="/show/:id"></Route>
+          <Route path="/movie/:id"></Route>
+          <Route path="/cast/:id"></Route>
+          <Route path="/search-results"></Route>
+        </Switch>
+      </Router>
+    </>
   );
 };
